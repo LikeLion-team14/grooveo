0511e27166f223e6056b5edd4f8364a14ea7b3d3
JunTaeINC <juntea3523@naver.com>
2023-05-24 15:41:42 +0900

    init

---
+++ src/main/java/com/kl/grooveo/GrooveoApplication.java
@@ -0,0 +1,13 @@
+package com.kl.grooveo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooveoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooveoApplication.class, args);
+	}
+
+}
