341c0c6856b02acbfd30f02f2f09861cdbb6c7b7
JunTaeINC <juntea3523@naver.com>
2023-05-25 23:44:20 +0900

    fix : JPA auditing 활성화

--- src/main/java/com/kl/grooveo/GrooveoApplication.java
+++ src/main/java/com/kl/grooveo/GrooveoApplication.java
@@ -2,7 +2,9 @@ package com.kl.grooveo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GrooveoApplication {
 
