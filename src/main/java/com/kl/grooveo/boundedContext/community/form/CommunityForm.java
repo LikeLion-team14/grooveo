9f9306d3ed4970dfeaef0ab9f33f94a3b09d71ef
JunTaeINC <juntea3523@naver.com>
2023-05-28 15:18:41 +0900

    style : 코드 정리

--- src/main/java/com/kl/grooveo/boundedContext/community/form/CommunityForm.java
+++ src/main/java/com/kl/grooveo/boundedContext/community/form/CommunityForm.java
@@ -8,12 +8,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CommunityForm {
-    @NotEmpty(message="제목은 필수항목입니다.")
-    @Size(max=200)
+    @NotEmpty(message = "제목은 필수항목입니다.")
+    @Size(max = 200)
     private String title;
 
     private String category;
 
-    @NotEmpty(message="내용은 필수항목입니다.")
+    @NotEmpty(message = "내용은 필수항목입니다.")
     private String content;
 }
