dafb5f384cc8dfd0c27fc55900eef9c367ec9a93
JunTaeINC <juntea3523@naver.com>
2023-05-29 16:27:53 +0900

    feat : 비밀번호 찾기 기능구현

---
+++ src/main/java/com/kl/grooveo/boundedContext/member/form/FindPasswordForm.java
@@ -0,0 +1,19 @@
+package com.kl.grooveo.boundedContext.member.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FindPasswordForm {
+
+    @NotBlank
+    @Size(min = 4, max = 30)
+    private final String username;
+
+    @NotBlank
+    @Size(min = 4, max = 30)
+    private final String email;
+}
