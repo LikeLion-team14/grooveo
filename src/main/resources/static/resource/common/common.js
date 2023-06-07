f3df1cae83ca34c51e4ce5570c266d465a08ab28
Genie16-github <4411dlwoqja@naver.com>
2023-06-07 15:58:26 +0900

    feat: 상단바, 게시글 리스트 UI 수정

--- src/main/resources/static/resource/common/common.js
+++ src/main/resources/static/resource/common/common.js
@@ -44,3 +44,18 @@ function toastWarning(msg) {
         toastr["warning"](pureMsg, "경고");
     }
 }
+
+function TopBar__init() {
+    $(window).scroll(function() {
+        let scrollTop = $(window).scrollTop();
+
+        if ( scrollTop == 0) {
+            $('.top-bar').removeClass('hover');
+        }
+        else {
+            $('.top-bar').addClass('hover');
+        }
+    });
+}
+
+TopBar__init();
