d3c9a98444b7037a927b1caed1d5d172a4f59a46
JunTaeINC <juntea3523@naver.com>
2023-06-08 23:07:10 +0900

    feat : EventAfterFollow 구현

--- src/main/java/com/kl/grooveo/base/event/EventAfterFollow.java
+++ src/main/java/com/kl/grooveo/base/event/EventAfterFollow.java
@@ -1,4 +1,16 @@
 package com.kl.grooveo.base.event;
 
-public class EventAfterFollow {
+import com.kl.grooveo.boundedContext.follow.entity.Follow;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class EventAfterFollow extends ApplicationEvent {
+
+    private final Follow follow;
+
+    public EventAfterFollow(Object source, Follow follow) {
+        super(source);
+        this.follow = follow;
+    }
 }
