ed500d7b9196bb9a49813aacfb74723c39d82fc1
JunTaeINC <juntea3523@naver.com>
2023-06-08 23:23:18 +0900

    feat : EventAfterUnFollow 구현

---
+++ src/main/java/com/kl/grooveo/base/event/EventAfterUnFollow.java
@@ -0,0 +1,16 @@
+package com.kl.grooveo.base.event;
+
+import com.kl.grooveo.boundedContext.follow.entity.Follow;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class EventAfterUnFollow extends ApplicationEvent {
+
+    private final Follow follow;
+
+    public EventAfterUnFollow(Object source, Follow follow) {
+        super(source);
+        this.follow = follow;
+    }
+}
