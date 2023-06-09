2df1bac3ff6eda9e906552ae44e55528710aeb65
JH <118197952+debussykim@users.noreply.github.com>
2023-06-09 15:36:09 +0900

    AWS S3로 파일 업로드 구현 (#9)
    * chore : aws 의존성 추가
    
    * feat : 파일에 대한 정보(음원명, 작곡가, 음원설명, 파일url)를 담는 FileInfo 엔티티 추가
    
    * feat : FileInfo에 대한 DB 작업을 위한 리포지터리 추가
    
    * feat : 파일 업로드 요청 수신, DB에 파일 메타데이터 저장하는 FileUploadController 추가
    
    * fix : 파일Url 경로 수정
    
    * feat : AWSS3Client 객체를 만들어 Bean으로 주입하는 AWSConfig 추가
    
    * chore : 경로 수정, secret 수정, 사용하지 않는 어노테이션 제거
    
    * chore : 도커 빌드 문제 부분 수정, prod 추가

---
+++ src/main/java/com/kl/grooveo/boundedContext/library/service/FileInfoService.java
@@ -0,0 +1,25 @@
+package com.kl.grooveo.boundedContext.library.service;
+
+import com.kl.grooveo.boundedContext.library.entity.FileInfo;
+import com.kl.grooveo.boundedContext.library.repository.FileInfoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileInfoService {
+
+    private final FileInfoRepository fileInfoRepository;
+
+    public FileInfoService(FileInfoRepository fileInfoRepository) {
+        this.fileInfoRepository = fileInfoRepository;
+    }
+
+    public FileInfo saveFileInfo(String title, String author, String description, String fileUrl) {
+        FileInfo fileInfo = new FileInfo();
+        fileInfo.setTitle(title);
+        fileInfo.setAuthor(author);
+        fileInfo.setDescription(description);
+        fileInfo.setFileUrl(fileUrl);
+
+        return fileInfoRepository.save(fileInfo);
+    }
+}
