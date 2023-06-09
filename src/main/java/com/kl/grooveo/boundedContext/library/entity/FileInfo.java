4477f4d790879f329b09fa52393418efa6427f45
debussykim <debussysuh@gmail.com>
2023-06-09 11:31:34 +0900

    feat : 파일에 대한 정보(음원명, 작곡가, 음원설명, 파일url)를 담는 FileInfo 엔티티 추가

---
+++ src/main/java/com/kl/grooveo/boundedContext/library/entity/FileInfo.java
@@ -0,0 +1,21 @@
+package com.kl.grooveo.boundedContext.library.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "file_info")
+public class FileInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String description;
+    private String fileUrl;
+
+}
