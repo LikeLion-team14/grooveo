a8d177c441303c02c31f434a8273684ce8c814cf
JunTaeINC <juntea3523@naver.com>
2023-06-10 16:26:47 +0900

    feat : UserLoginFailHandler 구현 / SecurityConfig 기능 추가

--- src/main/java/com/kl/grooveo/base/security/SecurityConfig.java
+++ src/main/java/com/kl/grooveo/base/security/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.kl.grooveo.base.security;
 
+import com.kl.grooveo.base.handler.UserLoginFailHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,8 +17,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final UserLoginFailHandler userLoginFailHandler;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,7 +29,10 @@ public class SecurityConfig {
                 .formLogin(
                         formLogin -> formLogin
                                 .loginPage("/usr/member/login")
+                                .failureHandler(userLoginFailHandler)
+                                .defaultSuccessUrl("/")
                 )
+
                 .oauth2Login(
                         oauth2Login -> oauth2Login
                                 .loginPage("/usr/member/login")
@@ -37,5 +48,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
