33acf97fbffa44270650f0646efa7a8f16d8e943
debussysuh <debussysuh@gmail.com>
2023-06-10 14:52:04 +0900

    chore : saveFileInfo를 객체로 받도록 수정

--- src/main/java/com/kl/grooveo/boundedContext/library/service/FileInfoService.java
+++ src/main/java/com/kl/grooveo/boundedContext/library/service/FileInfoService.java
@@ -13,12 +13,7 @@ public class FileInfoService {
         this.fileInfoRepository = fileInfoRepository;
     }
 
-    public FileInfo saveFileInfo(String title, String description, String fileUrl) {
-        FileInfo fileInfo = new FileInfo();
-        fileInfo.setTitle(title);
-        fileInfo.setDescription(description);
-        fileInfo.setFileUrl(fileUrl);
-
+    public FileInfo saveFileInfo(FileInfo fileInfo) {
         return fileInfoRepository.save(fileInfo);
     }
 }
