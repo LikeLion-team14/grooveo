0f73532f2506a2d55170d1193b5bb63fa36b5deb
Genie16-github <4411dlwoqja@naver.com>
2023-06-11 12:43:20 +0900

    feat: 댓글 앵커 추가

--- src/main/java/com/kl/grooveo/boundedContext/comment/controller/FreedomPostCommentController.java
+++ src/main/java/com/kl/grooveo/boundedContext/comment/controller/FreedomPostCommentController.java
@@ -12,13 +12,13 @@ import com.kl.grooveo.boundedContext.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequestMapping("/comment")
 @RequiredArgsConstructor
@@ -33,12 +33,12 @@ public class FreedomPostCommentController {
     @PostMapping("/create/{id}")
     public String create(Model model, @PathVariable("id") Long id,
                          @RequestParam(value = "commentPage", defaultValue = "0") int commentPage,
+                         @RequestParam(value = "so", defaultValue = "create") String so,
                          @Valid CommentForm commentForm, BindingResult bindingResult, ReplyForm replyForm) {
 
         FreedomPost freedomPost = freedomPostService.getFreedomPost(id);
         Member member = memberService.findByUsername(rq.getMember().getUsername()).orElseThrow();
-
-        Page<FreedomPostComment> commentPaging = this.freedomPostCommentService.getList(freedomPost, commentPage, "create");
+        Page<FreedomPostComment> commentPaging = getCommentPaging(freedomPost, commentPage, "create");
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("freedomPost", freedomPost);
@@ -46,21 +46,47 @@ public class FreedomPostCommentController {
             return "usr/community/freedomPost/detail";
         }
 
-        FreedomPostComment freedomPostComment = freedomPostCommentService.create(freedomPost, commentForm.getContent(), member);
-        return String.format("redirect:/community/freedomPost/detail/%s#comment-%s",
-                freedomPostComment.getFreedomPost().getId(), freedomPostComment.getId());
+        FreedomPostComment freedomPostComment = createFreedomPostComment(freedomPost, commentForm.getContent(), member);
+        commentPaging = getCommentPaging(freedomPost, commentPage, so);
+
+        if (so.equals("recent")) {
+            return createRedirectUrl(freedomPostComment.getFreedomPost().getId(), 0, so, freedomPostComment.getId());
+        }
+
+        return createRedirectUrl(freedomPostComment.getFreedomPost().getId(), commentPaging.getTotalPages() - 1, so, freedomPostComment.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id) {
-        FreedomPostComment freedomPostComment = this.freedomPostCommentService.getComment(id);
+        FreedomPostComment freedomPostComment = freedomPostCommentService.getComment(id);
 
         if (!freedomPostComment.getAuthor().getUsername().equals(rq.getMember().getUsername())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+            throw new AccessDeniedException("삭제권한이 없습니다.");
         }
 
-        this.freedomPostCommentService.delete(freedomPostComment);
-        return String.format("redirect:/community/freedomPost/detail/%s", freedomPostComment.getFreedomPost().getId());
+        freedomPostCommentService.delete(freedomPostComment);
+        return createDetailRedirectUrl(freedomPostComment.getFreedomPost().getId());
+    }
+
+    private Page<FreedomPostComment> getCommentPaging(FreedomPost freedomPost, int commentPage, String so) {
+        return freedomPostCommentService.getList(freedomPost, commentPage, so);
+    }
+
+    private FreedomPostComment createFreedomPostComment(FreedomPost freedomPost, String content, Member member) {
+        return freedomPostCommentService.create(freedomPost, content, member);
+    }
+
+    private String createRedirectUrl(Long postId, int commentPage, String so, Long commentId) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/community/freedomPost/detail/{postId}")
+                .queryParam("commentPage", commentPage)
+                .queryParam("so", so)
+                .fragment("comment-" + commentId);
+
+        return "redirect:" + builder.buildAndExpand(postId).toUriString();
+    }
+
+    private String createDetailRedirectUrl(Long postId) {
+        return "redirect:/community/freedomPost/detail/" + postId;
     }
 }
