68e4a863305930ff65cabfb8254a06174c47513b
JunTae_Kim <105341433+JunTaeINC@users.noreply.github.com>
2023-07-28 14:58:54 +0900

    UserDetailsService 디렉토리 이동 (config) / yml 수정 (#94)
    * replace: (CustomUserDetailsService) security 디렉토리로 이동
    
    * refactor: (application.yml) 민감정보 secret 으로 이동 / 통일화
    
    * style: 주석 제거

---
+++ src/main/java/com/kl/grooveo/base/security/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.kl.grooveo.base.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kl.grooveo.boundedContext.member.entity.Member;
+import com.kl.grooveo.boundedContext.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = false)
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final MemberRepository memberRepository;
+	private final String ROLE_ADMIN = "admin";
+	private final String ROLE_USER = "user";
+
+	@Override
+	public UserDetails loadUserByUsername(String username) {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		Member member = memberRepository.findByUsername(username).orElseThrow(
+			() -> new UsernameNotFoundException("username(%s) not found".formatted(username)));
+
+		if (ROLE_ADMIN.equals(username)) { // TODO: 추가 설정 필요
+			authorities.add(new SimpleGrantedAuthority(ROLE_ADMIN));
+		} else {
+			authorities.add(new SimpleGrantedAuthority(ROLE_USER));
+		}
+
+		return new User(member.getUsername(), member.getPassword(), authorities);
+	}
+}
